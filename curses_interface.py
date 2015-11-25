d52aeedffb480555934b4b4bd2653a3ac6aec822
Nicholas Mastrokalos <Nick@Nicholass-MacBook-Air.local>
2015-11-25 18:32:51 -0500

    new files with db validation

--- curses_interface.py
+++ curses_interface.py
@@ -27,38 +27,12 @@ class MyApp(object):
     # Connect to database
     con = connectToDatabase(self.screen)
 
-<<<<<<< HEAD
     # Menu starting point
     printMainMenu(self.screen, con)
 
-=======
-    # Collect db info (host ip, username, etc.)
-    un, addy, db, pw = getConnectionInfo(self.screen)
-    # un = "root"
-    # addy = "192.168.1.181"
-    # pw = "password"
-    # db = "TestDC"
->>>>>>> 968c38cf256d8c46dfb3950278cb8abb680d6084
 
 
-    if CON['state'] == 0:
-        # Good credentials, continue to menu starting point
-        printStatus(self.screen, CON['state'])
-        printMainMenu(self.screen)
 
-    else:
-        # Unsuccessful connnection
-        while CON['state'] != 0:
-            printStatus(self.screen, CON['state'])
-            # Bad credentials, try again until OK
-            un, addy, db, pw = getConnectionInfo(self.screen)
-            CON = database_queries.dbConnect(un, pw, db, addy)
-        else:
-            #Credentials finally validated, continue to main menu
-            printStatus(self.screen, CON['state'])
-            printMainMenu(self.screen)
-
-<<<<<<< HEAD
 
 # ==================================================
 # Name: 
@@ -152,115 +126,36 @@ def printMainMenu(stdscr, con):
     stdscr.addstr(2, 60, con['user'])
     stdscr.addstr(3, 2, "DATABASE NAME:")
     stdscr.addstr(3, 60, con['database'])
-=======
-#----------------------------------------------------
-def printStatus(stdscr, status):
 
-    stdscr.clear()
-    stdscr.border(0)
-    stdscr.addstr(6, 30, "-- STATUS MESSAGE --")
+    stdscr.addstr(4, 2, "----------------------------------------------------------------------------")
+    stdscr.addstr(6, 30, "-- MAIN MENU --")
 
-    if status == 0:
-        stdscr.addstr(11, 6, "CONNECTION SUCCESSFUL")
+    # Print main menu options
+    stdscr.addstr(9, 6, "[1]  View / Edit / Search Table")
+    stdscr.addstr(11, 6, "[2]  Create Table")
+    stdscr.addstr(13, 6, "[3]  Delete Table")
+    stdscr.addstr(15, 6, "[4]  About")
+    stdscr.addstr(17, 6, "[5]  Log Off / Exit")
+    stdscr.refresh()
 
-    else:
-        stdscr.addstr(9, 6, "CONNECTION FAILED")
-        stdscr.addstr(11, 6, "VERIFY YOUR CREDENTIALS AND TRY AGAIN")
+    # Collect user's navigation selection
+    x = stdscr.getch()
 
-    stdscr.addstr(13, 6, "PRESS ANY KEY TO CONTINUE")
-    justWait = stdscr.getch()
-
-
-def getConnectionInfo(stdscr):
-    stdscr.clear()
-    stdscr.border(0)    
-
-    if DEBUG:
-        username = "root"
-        host = "192.168.1.181"
-        password = "password"
-        database = "TestDC"
->>>>>>> 968c38cf256d8c46dfb3950278cb8abb680d6084
-
-    else:
-        # collect necessary connection info
-        stdscr.addstr(10, 5, "Enter the database host address:")
-        host = stdscr.getstr(10, 38, 15)
-        stdscr.clear()
-
-        stdscr.addstr(10, 5, "Enter the database username:")
-        username = stdscr.getstr(10, 38, 15)
-        stdscr.clear()
-
-        stdscr.addstr(10, 5, "Enter the database name:")
-        database = stdscr.getstr(10, 38, 15)
-        stdscr.clear()
-
-<<<<<<< HEAD
     # Navigate to submenu
     if x == ord('1'):
         printViewEditSearchSubmenu(stdscr, con)
-=======
-        stdscr.addstr(10, 5, "Enter the database password:")
-        password = stdscr.getstr(10, 38, 15)
-        stdscr.clear()
->>>>>>> 968c38cf256d8c46dfb3950278cb8abb680d6084
 
-    return username, host, database, password
+    if x == ord('2'):
+        curses.endwin()
 
+    if x == ord('3'):
+        curses.endwin()
 
-<<<<<<< HEAD
     if x == ord('4'):
         printAboutSubmenu(stdscr, con)
-=======
-def printMainMenu(stdscr):
-    x = 0
-    while x != ord('5'):
->>>>>>> 968c38cf256d8c46dfb3950278cb8abb680d6084
 
-        stdscr.clear()
-        stdscr.border(0)
-    
-        # Print main menu header information
-        stdscr.addstr(1, 2, "HOST IP:")
-    #TODO:  <------- INSERT CODE HERE FOR OBTAINING IP--------------->
-        stdscr.addstr(1, 60, "XXX.XXX.XXX.XXX")
-        stdscr.addstr(2, 2, "DB USER:")
-    #TODO:  <------- INSERT CODE HERE FOR OBTAINING USER NAME--------------->
-        stdscr.addstr(2, 60, "NAME_HERE")
-        stdscr.addstr(3, 2, "DATABASE NAME:")
-    #TODO:  <------- INSERT CODE HERE FOR OBTAINING DATABASE--------------->
-        stdscr.addstr(3, 60, "DATABASE_NAME")
-
-        stdscr.addstr(4, 2, "----------------------------------------------------------------------------")
-        stdscr.addstr(6, 30, "-- MAIN MENU --")
-
-        # Print main menu options
-        stdscr.addstr(9, 6, "[1]  View / Edit / Search Table")
-        stdscr.addstr(11, 6, "[2]  Create Table")
-        stdscr.addstr(13, 6, "[3]  Delete Table")
-        stdscr.addstr(15, 6, "[4]  About")
-        stdscr.addstr(17, 6, "[5]  Log Off / Exit")
-        stdscr.refresh()
-
-        # Collect user's navigation selection
-        x = stdscr.getch()
-
-        # Navigate to submenu
-        if x == ord('1'):
-            printViewEditSearchSubmenu(stdscr)
-
-        if x == ord('2'):
-            curses.endwin()
-
-        if x == ord('3'):
-            curses.endwin()
-
-        if x == ord('4'):
-            printAboutSubmenu(stdscr)
-
-    #    if x == ord('5'):
-    curses.endwin()
+    if x == ord('5'):
+        curses.endwin()
 
 # TODO: need to refactor with hightlight selection
 
@@ -487,8 +382,4 @@ def printLogOffSubMenu(stdscr, con):
     stdscr.addstr(12, 24, "LOGGING OFF...")
     time.sleep(5)
 # TODO: log off of database
-<<<<<<< HEAD
     curses.endwin()
-=======
-    curses.endwin()    
->>>>>>> 968c38cf256d8c46dfb3950278cb8abb680d6084
