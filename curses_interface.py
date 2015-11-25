968c38cf256d8c46dfb3950278cb8abb680d6084
Darnel Clayton <darnelclayton@gmail.com>
2015-11-25 13:21:50 -0500

    Modified UI, added screen for status

--- curses_interface.py
+++ curses_interface.py
@@ -21,91 +21,127 @@ class MyApp(object):
 
     # Collect db info (host ip, username, etc.)
     un, addy, db, pw = getConnectionInfo(self.screen)
-
+    # un = "root"
+    # addy = "192.168.1.181"
+    # pw = "password"
+    # db = "TestDC"
 
 # TODO: establish connection with database and either quit or retry with failure
     CON = database_queries.dbConnect(un, pw, db, addy) 
 
+    if CON['state'] == 0:
+        # Good credentials, continue to menu starting point
+        printStatus(self.screen, CON['state'])
+        printMainMenu(self.screen)
 
-    # Menu starting point
-    printMainMenu(self.screen)
+    else:
+        # Unsuccessful connnection
+        while CON['state'] != 0:
+            printStatus(self.screen, CON['state'])
+            # Bad credentials, try again until OK
+            un, addy, db, pw = getConnectionInfo(self.screen)
+            CON = database_queries.dbConnect(un, pw, db, addy)
+        else:
+            #Credentials finally validated, continue to main menu
+            printStatus(self.screen, CON['state'])
+            printMainMenu(self.screen)
+
 #----------------------------------------------------
+def printStatus(stdscr, status):
+
+    stdscr.clear()
+    stdscr.border(0)
+    stdscr.addstr(6, 30, "-- STATUS MESSAGE --")
+
+    if status == 0:
+        stdscr.addstr(11, 6, "CONNECTION SUCCESSFUL")
+
+    else:
+        stdscr.addstr(9, 6, "CONNECTION FAILED")
+        stdscr.addstr(11, 6, "VERIFY YOUR CREDENTIALS AND TRY AGAIN")
+
+    stdscr.addstr(13, 6, "PRESS ANY KEY TO CONTINUE")
+    justWait = stdscr.getch()
 
 
 def getConnectionInfo(stdscr):
     stdscr.clear()
-    stdscr.border(0)
-
-    # collect necessary connection info
-    stdscr.addstr(10, 5, "Enter the database host address:")
-    host = stdscr.getstr(10, 38, 15)
-    stdscr.clear()
-
-    stdscr.addstr(10, 5, "Enter the database username:")
-    username = stdscr.getstr(10, 38, 15)
-    stdscr.clear()
-
-    stdscr.addstr(10, 5, "Enter the database name:")
-    database = stdscr.getstr(10, 38, 15)
-    stdscr.clear()
-
-    stdscr.addstr(10, 5, "Enter the database password:")
-    password = stdscr.getstr(10, 38, 15)
-    stdscr.clear()
+    stdscr.border(0)    
 
     if DEBUG:
         username = "root"
-        host = "192.168.10.121"
+        host = "192.168.1.181"
         password = "password"
-        database = "testDB"
+        database = "TestDC"
+
+    else:
+        # collect necessary connection info
+        stdscr.addstr(10, 5, "Enter the database host address:")
+        host = stdscr.getstr(10, 38, 15)
+        stdscr.clear()
+
+        stdscr.addstr(10, 5, "Enter the database username:")
+        username = stdscr.getstr(10, 38, 15)
+        stdscr.clear()
+
+        stdscr.addstr(10, 5, "Enter the database name:")
+        database = stdscr.getstr(10, 38, 15)
+        stdscr.clear()
+
+        stdscr.addstr(10, 5, "Enter the database password:")
+        password = stdscr.getstr(10, 38, 15)
+        stdscr.clear()
 
     return username, host, database, password
 
 
 def printMainMenu(stdscr):
-    stdscr.clear()
-    stdscr.border(0)
+    x = 0
+    while x != ord('5'):
+
+        stdscr.clear()
+        stdscr.border(0)
     
-    # Print main menu header information
-    stdscr.addstr(1, 2, "HOST IP:")
-#TODO:  <------- INSERT CODE HERE FOR OBTAINING IP--------------->
-    stdscr.addstr(1, 60, "XXX.XXX.XXX.XXX")
-    stdscr.addstr(2, 2, "DB USER:")
-#TODO:  <------- INSERT CODE HERE FOR OBTAINING USER NAME--------------->
-    stdscr.addstr(2, 60, "NAME_HERE")
-    stdscr.addstr(3, 2, "DATABASE NAME:")
-#TODO:  <------- INSERT CODE HERE FOR OBTAINING DATABASE--------------->
-    stdscr.addstr(3, 60, "DATABASE_NAME")
+        # Print main menu header information
+        stdscr.addstr(1, 2, "HOST IP:")
+    #TODO:  <------- INSERT CODE HERE FOR OBTAINING IP--------------->
+        stdscr.addstr(1, 60, "XXX.XXX.XXX.XXX")
+        stdscr.addstr(2, 2, "DB USER:")
+    #TODO:  <------- INSERT CODE HERE FOR OBTAINING USER NAME--------------->
+        stdscr.addstr(2, 60, "NAME_HERE")
+        stdscr.addstr(3, 2, "DATABASE NAME:")
+    #TODO:  <------- INSERT CODE HERE FOR OBTAINING DATABASE--------------->
+        stdscr.addstr(3, 60, "DATABASE_NAME")
 
-    stdscr.addstr(4, 2, "----------------------------------------------------------------------------")
-    stdscr.addstr(6, 30, "-- MAIN MENU --")
+        stdscr.addstr(4, 2, "----------------------------------------------------------------------------")
+        stdscr.addstr(6, 30, "-- MAIN MENU --")
 
-    # Print main menu options
-    stdscr.addstr(9, 6, "[1]  View / Edit / Search Table")
-    stdscr.addstr(11, 6, "[2]  Create Table")
-    stdscr.addstr(13, 6, "[3]  Delete Table")
-    stdscr.addstr(15, 6, "[4]  About")
-    stdscr.addstr(17, 6, "[5]  Log Off / Exit")
-    stdscr.refresh()
+        # Print main menu options
+        stdscr.addstr(9, 6, "[1]  View / Edit / Search Table")
+        stdscr.addstr(11, 6, "[2]  Create Table")
+        stdscr.addstr(13, 6, "[3]  Delete Table")
+        stdscr.addstr(15, 6, "[4]  About")
+        stdscr.addstr(17, 6, "[5]  Log Off / Exit")
+        stdscr.refresh()
 
-    # Collect user's navigation selection
-    x = stdscr.getch()
+        # Collect user's navigation selection
+        x = stdscr.getch()
 
-    # Navigate to submenu
-    if x == ord('1'):
-        printViewEditSearchSubmenu(stdscr)
+        # Navigate to submenu
+        if x == ord('1'):
+            printViewEditSearchSubmenu(stdscr)
 
-    if x == ord('2'):
-        curses.endwin()
+        if x == ord('2'):
+            curses.endwin()
 
-    if x == ord('3'):
-        curses.endwin()
+        if x == ord('3'):
+            curses.endwin()
 
-    if x == ord('4'):
-        printAboutSubmenu(stdscr)
+        if x == ord('4'):
+            printAboutSubmenu(stdscr)
 
-    if x == ord('5'):
-        curses.endwin()
+    #    if x == ord('5'):
+    curses.endwin()
 
 # TODO: need to refactor with hightlight selection
 
